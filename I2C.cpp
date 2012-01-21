2157699453df02d6829a620ad3e51b1baf7473ef
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-01-21 10:48:44 +0200

    rename & fix files for arduino 1.0

--- I2C.cpp
+++ I2C.cpp
@@ -31,7 +31,7 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "WProgram.h"
+#include "Arduino.h"
 #include <inttypes.h>
 #include "I2C.h"
 
--- I2C.h
+++ I2C.h
@@ -31,7 +31,7 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "WProgram.h"
+#include "Arduino.h"
 #include <inttypes.h>
 
 #ifndef I2C_h
