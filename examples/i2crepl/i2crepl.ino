9a1d5924499b6e0f4d2bbebc5e14ebaff190c91b
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-08-12 13:10:30 +0300

    the state machine compiles, whether it works is another question...

--- examples/i2crepl/i2crepl.ino
+++ examples/i2crepl/i2crepl.ino
@@ -82,10 +82,72 @@ enum parser_states {
     in_hex,
     p_idle,
 };
+
+inline boolean is_hex_char(byte current_char)
+{
+    if (   (   current_char >= 0x30
+            && current_char <= 0x39) // 0-9
+        || (   current_char >= 0x61
+            && current_char <= 0x66) // a-f
+        || (   current_char >= 0x41
+            && current_char <= 0x46) // A-F
+       )
+    {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Parses ASCII [0-9A-F] hexadecimal to byte value
+ */
+inline byte ardubus_hex2byte(byte hexchar)
+{
+    if (   0x40 < hexchar
+        && hexchar < 0x47) // A-F
+    {
+        return (hexchar - 0x41) + 10; 
+    }
+    if (   0x2f < hexchar
+        && hexchar < 0x3a) // 0-9
+    {
+        return (hexchar - 0x30);
+    }
+    return 0x0; // Failure.
+    
+}
+
+inline byte ardubus_hex2byte(byte hexchar0, byte hexchar1)
+{
+    return (ardubus_hex2byte(hexchar0) << 4) | ardubus_hex2byte(hexchar1);
+}
+
+inline int ardubus_hex2int(byte hexchar0, byte hexchar1, byte hexchar2, byte hexchar3)
+{
+    return ardubus_hex2byte(hexchar0, hexchar1) << 8 | ardubus_hex2byte(hexchar2, hexchar3);
+}
+
+inline byte parse_hex(char *parsebuffer)
+{
+    byte len = strlen(parsebuffer);
+    if (len > 2)
+    {
+        return 0x0;
+    }
+    if (len == 2)
+    {
+        return ardubus_hex2byte(parsebuffer[0], parsebuffer[1]);
+    }
+    return ardubus_hex2byte(parsebuffer[0]);
+}
+
 inline void process_command()
 {
+    char hexparsebuffer[5];
+    byte hexparsebuffer_i = 0;
     byte parser_state = p_idle;
     byte next_parser_state = p_idle; // might not be needed 
+    byte prev_parser_state = p_idle; // might not be needed 
     byte maxsize = strlen(incoming_command);
     for(byte i=0; i < maxsize; i++)
     {
@@ -94,14 +156,88 @@ inline void process_command()
         {
             case start_seen:
             {
+                prev_parser_state = parser_state;
+                if (current_char == 0x20) // space
+                {
+                    parser_state = p_idle;
+                }
+                else if (is_hex_char(current_char)) // Allow also hex directly after start
+                {
+                    parser_state = in_hex;
+                    hexparsebuffer[hexparsebuffer_i++] = current_char; 
+                }
+                else
+                {
+                    // Invalid char
+                    Serial.print("Invalid character in position ");
+                    Serial.print(i, DEC);
+                    Serial.println(" when parsing command");
+                    return;
+                }
             }
                 break;
             case stop_seen:
             {
+                // after stop we only allow space to return to idle
+                if (current_char == 0x20) // space
+                {
+                    prev_parser_state = parser_state;
+                    parser_state = p_idle;
+                }
+                else
+                {
+                    // Invalid char
+                    Serial.print("Invalid character in position ");
+                    Serial.print(i, DEC);
+                    Serial.println(" when parsing command");
+                    return;
+                }
             }
                 break;
             case in_hex:
             {
+                if (is_hex_char(current_char))
+                {
+                    hexparsebuffer[hexparsebuffer_i++] = current_char;
+                    if (hexparsebuffer_i > 2)
+                    {
+                        Serial.println("Can only have byte wide hex strings");
+                        return;
+                    }
+                }
+                else if (current_char == 0x20) // space
+                {
+                    byte parsed_byte = parse_hex(&hexparsebuffer[0]);
+                    byte stat;
+                    if (prev_parser_state == start_seen)
+                    {
+                        stat = I2c.sendAddress(parsed_byte);
+                        Serial.print("sendAddress");
+                    }
+                    else
+                    {
+                        stat = I2c.sendByte(parsed_byte);
+                        Serial.print("sendByte");
+                    }
+                    Serial.print("(0x");
+                    Serial.print(parsed_byte, HEX);
+                    Serial.print(") returned: ");
+                    Serial.println(stat, DEC);
+                    // Clear buffer
+                    memset(&hexparsebuffer, 0, sizeof(hexparsebuffer));
+                    hexparsebuffer_i = 0;
+                    // Return state to idle
+                    prev_parser_state = parser_state;
+                    parser_state = p_idle;
+                }
+                else
+                {
+                    // Invalid char
+                    Serial.print("Invalid character in position ");
+                    Serial.print(i, DEC);
+                    Serial.println(" when parsing command");
+                    return;
+                }
             }
                 break;
             case p_idle:
@@ -110,27 +246,25 @@ inline void process_command()
                 {
                     case 0x5b: // ASCII "[", our start signifier
                     {
-                        I2c.start();
+                        byte stat = I2c.start();
                         parser_state = start_seen;
+                        Serial.print("START returned ");
+                        Serial.println(stat, DEC);
                     }
                         break;
                     case 0x5d: // ASCII "]", our stop signifier
                     {
-                        I2c.stop();
+                        byte stat = I2c.stop();
+                        Serial.print("STOP returned ");
+                        Serial.println(stat, DEC);
                         parser_state = stop_seen;
                     }
                         break;
                 }
-                if (   (   current_char >= 0x30
-                        && current_char <= 0x39) // 0-9
-                    || (   current_char >= 0x61
-                        && current_char <= 0x66) // a-f
-                    || (   current_char >= 0x41
-                        && current_char <= 0x46) // A-F
-                   )
+                if (is_hex_char(current_char))
                 {
-                    
                     parser_state = in_hex;
+                    hexparsebuffer[hexparsebuffer_i++] = current_char; 
                 }
             }
                 break;
